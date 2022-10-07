928c0a5d1c96497c41fcde7a10994b55fa119bf4
iamrebecas <rsolano9420@gmail.com>
2022-10-06 20:02:43 -0600

    Creacion de repositorio para tareas

---
+++ src/main/java/DesaWebyPatro_Tareas/DesaWebyPatroTareasApplication.java
@@ -0,0 +1,13 @@
+package DesaWebyPatro_Tareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesaWebyPatroTareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesaWebyPatroTareasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DesaWebyPatro_Tareas/DesaWebyPatroTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package DesaWebyPatro_Tareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesaWebyPatroTareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
